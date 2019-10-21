e21cddfd8b65d9addaef78d943d060d721d95afe
vladimirkondratev <kondrvl@yandex.ru>
2019-10-21 15:41:15 +0300

    3_13 spring-db

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,25 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+
+DELETE
+FROM meals;
+
+DELETE
+FROM users;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, user_id, date_time, description, calories)
+VALUES (100002, 100000, '31.10.2005 14:00', 'обед', 500),
+       (100003, 100000, '31.10.2005 20:00', 'ужин', 1000),
+       (100004, 100001, '31.10.2005 7:00', 'завтрак', 300),
+       (100005, 100001, '31.10.2005 13:30', 'обед', 800),
+       (100006, 100001, '31.10.2005 18:00', 'ужин', 1000);
